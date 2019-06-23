e42cd0a593d084ff9b6287bfac6766385c4213a1
Elena Fodorová <elfocz@infoconsulting.eu>
2019-06-23 21:47:48 +0200

    Edited Grid Container - centered Weather Items.

--- src/components/pages/MainPage/GridContainer.js
+++ src/components/pages/MainPage/GridContainer.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const GridContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
+  justify-items: center;
   margin: 20px 35px;
 `;
 

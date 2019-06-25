0cbfc09de5268746748ef6b2d5e1fd9adad02a6c
Elena Fodorová <elfocz@infoconsulting.eu>
2019-06-25 17:49:29 +0200

    Fixed Compare Link.

--- src/components/pages/MainPage/AqiItemWrapper.js
+++ src/components/pages/MainPage/AqiItemWrapper.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 
 const AqiItemWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 378px auto;
+
   @media (${props => props.theme.mediaQueries.tablet}) {
-    display: grid;
     grid-template-columns: 308px 102px;
     justify-content: center;
   }
--- src/components/pages/MainPage/NavLink.js
+++ src/components/pages/MainPage/NavLink.js
@@ -4,10 +4,14 @@ import { Link } from "react-router-dom";
 const NavLink = styled(Link)`
   text-decoration: none;
   text-align: right;
-  margin: 10px 0;
+  margin: 10px 35px;
   color: ${props =>
     props.theme.colors.darkSecondaryColor2 ||
     props.theme.colors.secondaryColor2};
-`;
 
+  @media (${props => props.theme.mediaQueries.tablet}) {
+    margin: 10px 0px;
+  }
+`;
+///
 export default NavLink;

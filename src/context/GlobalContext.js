78eb4edc9509b2869dc9f3adfbfe7a16a0faab89
Elena Fodorová <elfocz@infoconsulting.eu>
2019-07-05 16:45:10 +0200

    Added new data file  data.js

--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -1,27 +1,28 @@
 import React, { useState, createContext } from "react";
-import Cities from "../data/data.json";
+//import Cities from "../data/data.json";
+import Cities from "../data/data.js";
 //import Cities from "../data/data-small.json";
 
 export const GlobalContext = createContext();
 
 export const GlobalProvider = props => {
-  let cityList = [];
-  let id = 0;
-  Cities.forEach(country => {
-    country.states.forEach(state => {
-      state.cities.forEach(city => {
-        cityList.push({
-          id: id,
-          city: city,
-          state: state.state,
-          country: country.country
-        });
-        id++;
-      });
-    });
-  });
+  // let cityList = [];
+  // let id = 0;
+  // Cities.forEach(country => {
+  //   country.states.forEach(state => {
+  //     state.cities.forEach(city => {
+  //       cityList.push({
+  //         id: id,
+  //         city: city,
+  //         state: state.state,
+  //         country: country.country
+  //       });
+  //       id++;
+  //     });
+  //   });
+  // });
 
-  const [cities] = useState([cityList]);
+  const [cities] = useState([Cities]);
   return (
     <GlobalContext.Provider value={cities}>
       {props.children}

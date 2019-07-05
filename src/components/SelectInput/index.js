3f41ac570e967ad532dfc16e269531d0faf524ac
Elena Fodorová <elfocz@infoconsulting.eu>
2019-07-05 17:43:48 +0200

    Fixed fetching weather.

--- src/components/SelectInput/index.js
+++ src/components/SelectInput/index.js
@@ -52,6 +52,7 @@ const SelectInput = props => {
         <OptionList
           setInputValue={setInputValue}
           setOptionList={setOptionList}
+          inputText={inputValue}
           second={props.second}
         />
       ) : null}
